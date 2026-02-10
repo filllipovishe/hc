c4b705952d2ae8a848e59ff6c6285e52c3e6fa8a
filllipovishe <fill.lipovishe@gmail.com>
2026-02-10 14:51:47 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+              base: '/hc/',                    // ← это самая важная строка, которую мы добавили
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
